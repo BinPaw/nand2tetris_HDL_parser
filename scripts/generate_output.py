22ee43948fd7f788a0ff5a105d9d147e28908f64
BinPaw <ginadareishvili@gmail.com>
2025-07-27 17:36:36 +0400

    output generation done

--- scripts/generate_output.py
+++ scripts/generate_output.py
@@ -13,11 +13,25 @@ def generate_output_file(input_file: Path, output_file: Path):
     lines = f.readlines()
   
   vars = [var.strip() for var in lines[0].split('|')[1:-1]]
+  result = {k: [] for k in vars}
   for line in lines[1:]:
     vals = [False if val.strip() == '0' else True for val in line.split('|')[1:-1]]
     input = {vars[i]: vals[i] for i in range(len(vars))}
     output = runner.generate_output(name, input)
-    print(output)
+
+    cur_result = input.copy()
+    for k, v in output.items():
+      cur_result[k] = v
+    cur_result = {k: 1 if v else 0 for k, v in cur_result.items()}
+
+    for k, v in cur_result.items():
+      result[k].append(v)
+
+  with open(output_file, 'w') as f:
+    f.write(f'|{'|'.join([key if len(key) > 1 else f' {key} ' for key in result.keys()])}|\n')
+    for i in range(len(lines) - 1):
+      f.write(f'|{'|'.join([f' {result[k][i]} ' for k in result.keys()])}|\n')
+    
 
 def generate_output(input_dir: str, output_dir: str) -> None:
   input_dir = Path(input_dir)
